d0eceaa3a48ee502268848d7c98c2d31e1aa5ed8
MemoryOfStars <38707974+MemoryOfStars@users.noreply.github.com>
2019-12-01 16:17:29 +0900

    complete huffman

--- Huffman/Huffman/Huffman.cpp
+++ Huffman/Huffman/Huffman.cpp
@@ -1,8 +1,8 @@
 #include "huffman.h"
 //left smaller
-bool compare(pair<string, int> a, pair<string, int> b)
+bool compare(tree_node* a, tree_node* b)
 {
-	return a.second < b.second;
+	return a->weight < b->weight;
 }
 tree_node* huffman::get_root()
 {
@@ -17,45 +17,33 @@ tree_node* huffman::get_root()
 ***************************************/
 huffman::huffman(vector<pair<string, int>> freq)
 {
-	sort(freq.begin(),freq.end(), compare);     //order the frequncy map
-	vector<pair<string, int>>::iterator iter;
-	tree_node* subroot = NULL;
-	tree_node* new_subroot = NULL;
-
-	iter = freq.begin();
-	tree_node* left = new tree_node;
-	tree_node* right = new tree_node;
-	left->left = NULL; left->right = NULL; left->weight = iter->second; left->word = iter->first;
-	iter++;
-	right->left = NULL; right->right = NULL; right->weight = iter->second; right->word = iter->first;
-	subroot = new tree_node;
-	subroot->left = left;
-	subroot->right = right;
-	subroot->weight = left->weight + right->weight;
-	subroot->word = "";
-	iter++;
-	for (; iter != freq.end(); iter++)
+	tree_node* freq_iter = NULL;
+	vector<tree_node*> node_list;
+	for (int i = 0; i < freq.size(); i++)
 	{
-		new_subroot = new tree_node;
-		tree_node* temp = new tree_node;
-		temp->left = NULL; temp->right = NULL; temp->word = iter->first; temp->weight = iter->second;
-		if (iter->second <= subroot->weight)
-		{
-			new_subroot->left = temp;
-			new_subroot->right = subroot;
-			new_subroot->weight = new_subroot->left->weight + new_subroot->right->weight;
-		}
-		else 
-		{
-			new_subroot->left = subroot;
-			new_subroot->right = temp;
-			new_subroot->weight = new_subroot->left->weight + new_subroot->right->weight;
-		}
-		subroot = new_subroot;
-		new_subroot = NULL;
+		freq_iter = new tree_node;
+		freq_iter->left   = NULL;           freq_iter->right = NULL; 
+		freq_iter->weight = freq[i].second; freq_iter->word  = freq[i].first;
+		node_list.push_back(freq_iter);
 	}
-
-	huff_root = subroot;
+	tree_node* left = NULL;
+	tree_node* right = NULL;
+	tree_node* subtree = NULL;
+	while (node_list.size() > 1)
+	{
+		sort(node_list.begin(), node_list.end(), compare);
+		left = node_list[0];
+		right = node_list[1];
+		node_list.erase(node_list.begin());
+		node_list.erase(node_list.begin());
+		subtree = new tree_node;
+		subtree->left = left; subtree->right = right;
+		subtree->weight = left->weight + right->weight;
+		subtree->word = "";
+		node_list.push_back(subtree);
+	}
+	
+	huff_root = subtree;
 }
 
 void huffman::get_code(tree_node* root, string code)
